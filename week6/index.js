95ff0b15912d4fc62dc74eda9fd6bda893e9441d
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-07-16 20:07:44 +0530

    🚀 socket.io doc's chat app

---
+++ week6/index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
