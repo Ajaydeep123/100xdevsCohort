1f610eeeebb3245be8e1f9b397ba68e02aacc72e
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-08-02 00:28:34 +0530

    🚀

--- JS_dataStructure/stack.js
+++ JS_dataStructure/stack.js
@@ -32,6 +32,45 @@ function Stack(){
    }
 }
 
+// stack
+ function Stack(){
+   var items = [];
+   var top = 0;
+   //other methods go here
+
+   //Push an item in the Stack
+   this.push = function(element){
+     items[top++] = element
+   }
+   //top++, first performs the operation then increment's the value     
+
+   //Pop an item from the Stack
+   this.pop = function(){
+     return items[--top];
+   }
+   //--top, first decrements the value then performs the operation
+     
+   //Peek top item from the Stack
+   this.peek = function(){
+     return items[top - 1];
+   }
+
+   //Is Stack empty
+   this.isEmpty = function(){
+     return top === 0;
+   }     
+
+   //Clear the Stack
+   this.clear = function(){
+      top = 0;
+   }
+     
+   //Size of the Stack
+   this.size = function(){
+     return top;
+   }
+ }
+
 
 
 class Stack{
